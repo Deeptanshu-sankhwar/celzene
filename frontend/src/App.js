2c80177492dbe57091952564028ad84f37d7d1b4
Deeptanshu Sankhwar <deeptanshu@walletsocket.com>
2024-07-03 15:37:49 +0530

    kul2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,32 @@ import './App.css';
 import CounterApp from './pages/CounterApp';
 import Task from './pages/Task';
 import Signup from './pages/Signup';
+import Login from './pages/Login';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 // This app is a counter application
 
 // in HTML, we use "class", but in JSX, we use "className"
+
+
+// Planning for the frontend of our Library Management System
+// [X] Signup/Login Screen
+// 2. Navbar
+// 3. There should be a screen where the admin can see all the users of the library, there should be a button for admin to remove (ban) some users
+// 4. A screen with table that show all the books
+// 5. Displaying images and details of books, we will also show that the book is isseued or not
+// 6. There should be some UI to delete the book
+// 7. There should be some UI to issue the book to the student
+// 8. There should be some UI for the student to return the book
+// 9. Admin should see a screen where he/she can send reminders to students who have issued the book
+// 10. Screen where user can edit his/her name, password etc? (User Management Module)
+// 11. There should be a screen for the student to view the book he has currently issued
+// 12. Search bar which will search for books
+// 13. Dropdown which shows all the books
+// 14. Upload the book image
+// 15. Responsiveness (Screen should be readable and understandable across all devices) - Layout [Responsive Layout]
+
+
 
 function App() {  // App component - This is ultimate parent component of my website! App component has no parent component, it only has children
   
@@ -17,11 +39,17 @@ function App() {  // App component - This is ultimate parent component of my web
 
 
   return (
-    <div className="App">
-      {/* <CounterApp /> */}
-      {/* <Task /> */}
-      <Signup />
-    </div>
+    <Router>
+        <div className="App">
+        <Routes>
+          <Route path="/" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/todo" element={<Task />} />
+          <Route path="/counter" element={<CounterApp />} />
+        </Routes>
+      </div>
+
+      </Router>
   );
 }
 
---
+++ frontend/src/pages/Login.js
@@ -0,0 +1,49 @@
+import { useState } from "react";
+import axios from 'axios';  // npm install axios
+
+function Login()   {
+
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handlePasswordChange = (e) => {
+        setPassword(e.target.value)
+    }
+
+
+    const callLogInApi = async () => {
+        const payload = {
+            email: email,
+            password: password
+        } // this is my object which I will be passing in the body of my request to be sent to the backend server
+
+        // Now I will creat a POST HTTP call using Axios library where I will pass the payload object in the request
+        const response = await axios.post('http://localhost:4000/api/login', payload)
+        console.log(response)
+    }
+
+    return (
+        <div>
+
+            <div>
+                <p>Enter your email</p>
+                <input type="text" value={email} onChange={handleEmailChange}></input>
+            </div>
+
+            <div>
+                <p>Enter your password</p>
+                <input type="password" value={password} onChange={handlePasswordChange}></input>
+            </div>
+
+            <button onClick={callLogInApi}>Login</button>
+
+        </div>
+    )
+}
+
+export default Login
