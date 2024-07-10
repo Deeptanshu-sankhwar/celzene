2dd104b38809b22e3b5fcd4aeb3bb9eca421fc84
Deeptanshu Sankhwar <deeptanshu@walletsocket.com>
2024-07-10 15:41:14 +0530

    rseponsive

--- frontend/src/components/Navbar/navbar.js
+++ frontend/src/components/Navbar/navbar.js
@@ -1,39 +1,52 @@
+import { useState, useEffect } from 'react'
 import { Link } from "react-router-dom"
 import './navbar.css';
 
 function Navbar()   {
 
+    const width = window.innerWidth
+
+    const [showNav, setShowNav] = useState(false)
+
     return (
+        // need to check the size of the screen and accordingly render UI
         <nav className="navbar">
-            <ul>
-                <li>
-                    <Link to="/">Signup</Link>
-                </li>
-                <li>
-                    <Link to="/login">Login</Link>
-                </li>
-                {/* <li>
-                    <Link to="/todo">Todo</Link>
-                </li>
-                <li>
-                    <Link to="/counter">Counter</Link>
-                </li> */}
-                <li>
-                    <Link to="/showUsers">Show Users</Link>
-                </li>
-                <li>
-                    <Link to="/books">Show Books</Link>
-                </li>
-                <li>
-                    <Link to="/user-profile">User Profile</Link>
-                </li>
-                <li>
-                    <Link to="/snake">Snake Game</Link>
-                </li>
-                <li>
-                    <Link to="/pokemon">Pokemon</Link>
-                </li>
-            </ul>
+            {
+                (width < 960) && <button className="hamburger" onClick={() => setShowNav(!showNav)}>Toggle Navbar</button>
+            }
+            {
+                showNav && 
+                <ul>
+                    <li>
+                        <Link to="/">Signup</Link>
+                    </li>
+                    <li>
+                        <Link to="/login">Login</Link>
+                    </li>
+                    {/* <li>
+                        <Link to="/todo">Todo</Link>
+                    </li>
+                    <li>
+                        <Link to="/counter">Counter</Link>
+                    </li> */}
+                    <li>
+                        <Link to="/showUsers">Show Users</Link>
+                    </li>
+                    <li>
+                        <Link to="/books">Show Books</Link>
+                    </li>
+                    <li>
+                        <Link to="/user-profile">User Profile</Link>
+                    </li>
+                    <li>
+                        <Link to="/snake">Snake Game</Link>
+                    </li>
+                    <li>
+                        <Link to="/pokemon">Pokemon</Link>
+                    </li>
+                </ul>
+            }
+            
         </nav>
     )
 }
