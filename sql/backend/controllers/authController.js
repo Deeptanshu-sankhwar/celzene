78ced18cc2b8730354ad4473cf122c611d8748b9
Deeptanshu Sankhwar <deeptanshu@walletsocket.com>
2024-08-12 12:33:34 +0530

    hh

--- sql/backend/controllers/authController.js
+++ sql/backend/controllers/authController.js
@@ -2,15 +2,43 @@ const bcrypt = require('bcryptjs');
 const User = require('../models/userModel');
 const { generateToken } = require('../config/auth')
 
+const redis = require('redis');
+
+// create my redis connection cluster configs here
+const client = redis.createClient();
+// by default my redis client is gonna connect to 127.0.0.1, 6379, '', '', 0
+
+// Add a redis event listener which tells me whenever there is an error connecting to my redis cluster
+client.on('error', (err) => {
+    console.log("There was an error connecting to my redis cluster", err)
+})
+
+// connect to the redis cluster
+client.connect();
+
 exports.register = async (req, res) => {
+    // We are parsing the request body
     const { name, email, password, role, age } = req.body;
 
     try {
+        // We are hashing password
         const hashedPassword = await bcrypt.hash(password, 10)
+
+        // We are inserting a user inside SQL database
         User.create({name, email, password: hashedPassword, role, age}, async (err, user) => {
             if (err) {
                 console.error(err);
                 return res.status(500).json({ success: false, message: err.message });
+            }
+
+            // We need to insert this same user inside Redis as well
+            try {
+                await client.sAdd('users', {name, email, password: hashedPassword, role, age});
+            } catch (err)   {
+                return res.status(500).json({
+                    success: false,
+                    message: 'Error while inserting user inside Redis'
+                })
             }
 
             const token = generateToken(user);
